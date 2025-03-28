325927004ea0088bca406718664338fb4c34c637
Not_Real_Hacker() <greenmonkey@duck.com>
2025-03-27 20:11:31 -0400

    ini

---
+++ index.js
@@ -0,0 +1,23 @@
+window.alert(`Hacking... █`);
+window.alert(`Hacking... ██`);
+window.alert(`Hacking... ███`);
+window.alert(`Hacking... ████`);
+window.alert(`Hacking... █████`);
+window.alert(`Hacking... ██████`);
+window.alert(`Hacking... ███████`);
+window.alert(`Hacking... ████████`);
+window.alert(`Hacking... █████████`);
+window.alert(`Hacking... ██████████`);
+window.alert(`Hacking... ███████████`);
+window.alert(`Hacking... ████████████`);
+window.alert(`Hacking... █████████████`);
+window.alert(`Hacking... ██████████████`);
+window.alert(`Hacking... ███████████████`);
+window.alert(`Hacking... █████████████████████████`);
+window.alert(`Hacking... ███████████████████████████████████`);
+window.alert(`DONE...`);
+
+window.alert(`You have been hacked`);
+window.alert(`I have full access to your computer`);
+window.alert(`Subscribe to this YouTube channel or I will delete all your files`);
+window.alert(`https://youtube.com/@i_developer`);
